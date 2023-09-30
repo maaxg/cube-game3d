0bf3e4d2b1714da53ffa1857b9d10ee12ec7616c
maaxg <maxgps5@hotmail.com>
2023-09-30 17:22:21 -0300

    cube

---
+++ game.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+let renderer, scene, camera, cube;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+  cube = new THREE.Mesh(geometry, material);
+
+  camera.position.z = 25;
+
+  scene.add(cube, camera);
+}
+
+init();
+animate();
