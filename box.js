a00d6177faa08fe23e1f5633b9ae223643effcda
maaxg <maxgps5@hotmail.com>
2023-10-01 15:55:55 -0300

    feat: enemy generation

--- box.js
+++ box.js
@@ -1,6 +1,18 @@
 import * as THREE from "three";
-
 const GRAVITY = -0.002;
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
+
 class Box extends THREE.Mesh {
   constructor({
     width = 1,
@@ -9,6 +21,7 @@ class Box extends THREE.Mesh {
     color = 0x00ff00,
     velocity = { x: 0, y: 0, z: 0 },
     position = { x: 0, y: 0, z: 0 },
+    body = true,
   }) {
     super(
       new THREE.BoxGeometry(width, height, depth),
@@ -21,25 +34,96 @@ class Box extends THREE.Mesh {
 
     this.position.set(position.x, position.y, position.z);
 
+    this.updateSides();
+
+    this.velocity = velocity;
+    this.body = body;
+    if (this.body) this.movementListener();
+  }
+
+  movement() {
+    if (!this.body) return;
+    this.velocity.x = 0;
+
+    if (keys.a.pressed) this.velocity.x = -0.05;
+    if (keys.d.pressed) this.velocity.x = 0.05;
+    if (keys.space.pressed) this.velocity.y = 0.05;
+  }
+
+  movementListener() {
+    window.addEventListener("keydown", (ev) => {
+      console.log(ev.code);
+      switch (ev.code) {
+        case "KeyA":
+        case "ArrowLeft":
+          keys.a.pressed = true;
+          break;
+        case "KeyD":
+        case "ArrowRight":
+          keys.d.pressed = true;
+          break;
+        case "Space":
+          keys.space.pressed = true;
+          break;
+      }
+    });
+    window.addEventListener("keyup", (ev) => {
+      switch (ev.code) {
+        case "KeyA":
+        case "ArrowLeft":
+          keys.a.pressed = false;
+          break;
+        case "KeyD":
+        case "ArrowRight":
+          keys.d.pressed = false;
+          break;
+        case "Space":
+          keys.space.pressed = false;
+          break;
+      }
+    });
+  }
+
+  updateSides() {
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
     this.bottom = this.position.y - this.height / 2;
     this.top = this.position.y + this.height / 2;
 
-    this.velocity = velocity;
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
   }
 
   applyGravity({ ground }) {
     this.velocity.y += GRAVITY;
 
     // ground colision
-    if (this.bottom + this.velocity.y <= ground.top) {
-      this.velocity.y *= 0.8;
+    if (this.boxCollision({ box: this, box1: ground })) {
+      const friction = 0.5;
+      this.velocity.y *= friction;
       this.velocity.y = -this.velocity.y;
     } else this.position.y += this.velocity.y;
   }
 
-  update({ ground }) {
-    this.bottom = this.position.y - this.height / 2;
-    this.top = this.position.y + this.height / 2;
+  boxCollision({ box, box1 }) {
+    const xCollision = box.right >= box1.left && box.left <= box1.right;
+    const yCollision =
+      box.bottom + box.velocity.y <= box1.top && box.top >= box1.bottom;
+    const zCollision = box.front >= box1.back && box.back <= box1.front;
+
+    return xCollision && yCollision && zCollision;
+  }
+
+  update({ ground, enemy }) {
+    this.updateSides();
+
+    if (enemy) this.velocity.z += 0.0003;
+
+    this.position.x += this.velocity.x;
+    this.position.z += this.velocity.z;
+    // this.position.y += this.velocity.y;
+
     this.applyGravity({ ground });
   }
 }
--- game.js
+++ game.js
@@ -3,26 +3,40 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Box from "./box";
 
 let renderer, scene, camera, cube, ground;
-
+const enemies = [];
+let frames = 0;
+let spawnRate = 200;
 function animate() {
-  requestAnimationFrame(animate);
-  //  cube.position.y -= 0.01;
+  const animationId = requestAnimationFrame(animate);
+
+  cube.movement();
   cube.update({ ground });
+  enemies.forEach((enemy) => {
+    enemy.update({ ground, enemy: true });
+    if (enemy.boxCollision({ box: cube, box1: enemy })) {
+      console.log("collide");
+      cancelAnimationFrame(animationId);
+    }
+  });
+
+  frames++;
+  createEnemy();
 
   renderer.render(scene, camera);
 }
 
 function createGround() {
   ground = new Box({
-    width: 5,
+    width: 10,
     height: 0.5,
-    depth: 10,
+    depth: 50,
     color: 0x0000ff,
     position: {
       x: 0,
       y: -2,
       z: 0,
     },
+    body: false,
   });
 
   ground.receiveShadow = true;
@@ -30,17 +44,33 @@ function createGround() {
   scene.add(ground);
 }
 
-function createCube() {
-  cube = new Box({
-    velocity: {
-      x: 0,
-      y: -0.01,
-      z: 0,
-    },
-  });
-  cube.castShadow = true;
-
-  scene.add(cube);
+function createCube({
+  enemy = false,
+  position = { x: 0, y: 0, z: 0 },
+  velocity = {
+    x: 0,
+    y: -0.01,
+    z: 0,
+  },
+}) {
+  let enemyCube;
+  if (enemy) {
+    enemyCube = new Box({
+      velocity,
+      position,
+      color: enemy ? "red" : 0x00ff00,
+    });
+    enemyCube.castShadow = true;
+  } else {
+    cube = new Box({
+      velocity,
+      position,
+      color: enemy ? "red" : 0x00ff00,
+    });
+    cube.castShadow = true;
+  }
+  if (enemy) enemies.push(enemyCube);
+  scene.add(cube, enemyCube);
 }
 
 function initLight() {
@@ -50,6 +80,25 @@ function initLight() {
   light.castShadow = true;
 
   scene.add(light);
+}
+
+function createEnemy() {
+  if (frames % spawnRate === 0) {
+    if (spawnRate > 20) spawnRate -= 20;
+    createCube({
+      enemy: true,
+      position: {
+        x: (Math.random() - 0.5) * 10,
+        y: 0,
+        z: -20,
+      },
+      velocity: {
+        x: 0,
+        y: 0,
+        z: 0.005,
+      },
+    });
+  }
 }
 
 function init() {
@@ -71,7 +120,8 @@ function init() {
 
   initLight();
   createGround();
-  createCube();
+
+  createCube({});
 
   camera.position.z = 5;
 
